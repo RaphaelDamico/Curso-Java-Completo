5a059dad36da8ca8610d49b558d9e6639805e17c
RaphaelDamico <raphaelvivarinid@gmail.com>
2024-08-21 22:26:49 -0300

    feat: adapta problema de produtos para receber construtor com argumentos obrigatórios

--- ProblemaDeProdutoEmEstoque/src/application/Program.java
+++ ProblemaDeProdutoEmEstoque/src/application/Program.java
@@ -17,22 +17,25 @@ public class Program {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 		
-		Product p = new Product();
-		
 		System.out.println("Enter product data: ");
+		
 		System.out.print("Name: ");
-		p.name = sc.nextLine();
+		String name = sc.nextLine();
+		
 		System.out.print("Price: ");
-		p.price = sc.nextDouble();
+		double price = sc.nextDouble();
+		
 		System.out.print("Quantity in stock: ");
-		p.quantity = sc.nextInt();
+		Integer quantity = sc.nextInt();
+		
+		Product p = new Product(name, price, quantity);
 		
 		System.out.println();
 		System.out.println("Product data: " + p);
 		System.out.println();
 		
 		System.out.print("Enter the number of products to be added in stock: ");
-		Integer quantity = sc.nextInt();
+		quantity = sc.nextInt();
 		p.addProducts(quantity);
 		System.out.println();
 		System.out.print("Updated data: " + p);
--- ProblemaDeProdutoEmEstoque/src/entities/Product.java
+++ ProblemaDeProdutoEmEstoque/src/entities/Product.java
@@ -5,6 +5,12 @@ public class Product {
 	public double price;
 	public Integer quantity;
 	
+	public Product(String name, double price, Integer quantity) {
+		this.name = name;
+		this.price = price;
+		this.quantity = quantity;
+	}
+
 	public double totalValueInStock() {
 		return price * quantity;
 	}
